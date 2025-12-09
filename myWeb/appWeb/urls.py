713e570aaf1d2c51ddcde77e312bc566bc1d276c
Aitor López <aitor.lopron@gmail.com>
2025-12-09 23:13:45 +0100

    Ajustes genericos

--- myWeb/appWeb/urls.py
+++ myWeb/appWeb/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('certificado/<int:pk>', views.CertificadoDetailView.as_view(), name='certificado_detail'),
 
     # Url para contacto
-    path('contacto', views.InfoListView.as_view(), name='info'),
+    path('info', views.InfoListView.as_view(), name='info'),
 
     # Url para Login
     path('login', views.UserLoginView.as_view(), name='login'),
