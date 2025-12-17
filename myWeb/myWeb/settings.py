5418427784a214e1d5c43aa48bf05354d8a2df4d
Aitor López <aitor.lopron@gmail.com>
2025-12-17 18:38:48 +0100

    Config cloudinary

--- myWeb/myWeb/settings.py
+++ myWeb/myWeb/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'appWeb',
 ]
 
@@ -148,3 +150,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Redirección tras login o logout
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
+
+# Usa Cloudinary para todos los archivos subidos
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+import cloudinary
+import os
+
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+)
+
+# Configuración específica de django-cloudinary-storage
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
+    'FOLDER': 'MyWeb',  # Carpeta para esta app
+}
